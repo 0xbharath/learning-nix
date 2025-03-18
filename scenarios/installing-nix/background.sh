c552ecf9c87ebef4ef0471d96ba8285001dfd6b3
Bharath <bharath.isecure@pm.me>
2025-03-18 22:08:03 +0530

    Added prompt disable in installation

--- scenarios/installing-nix/background.sh
+++ scenarios/installing-nix/background.sh
@@ -2,4 +2,5 @@
 
 apt get update
 apt install -y curl tree
-curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
+curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --no-confirm
+. /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
