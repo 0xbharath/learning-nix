34275bb08c8e3d4fb9f987cd0fa949eb4ca549ca
Bharath <bharath.isecure@pm.me>
2025-03-18 22:15:20 +0530

    Updated Nix foreground script

--- scenarios/installing-nix/foreground.sh
+++ scenarios/installing-nix/foreground.sh
@@ -2,3 +2,4 @@
 
 curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --no-confirm
 . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+nix --version
