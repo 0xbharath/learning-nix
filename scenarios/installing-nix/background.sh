9b5db4099c60a0fbac0fc184dafb6f0ba2802fd5
Bharath <bharath.isecure@pm.me>
2025-03-18 22:10:03 +0530

    Added foreground.sh

--- scenarios/installing-nix/background.sh
+++ scenarios/installing-nix/background.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 apt get update
-apt install -y curl tree
-curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --no-confirm
-. /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+apt install -y curl
